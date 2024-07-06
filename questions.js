ee79c9756201f52654b459c817ae3e5be36da186
xDMich <149896755+xDMich@users.noreply.github.com>
2024-07-06 11:04:05 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,57 +1,29 @@
 questions =  [
     {"id": 0, "question": "People work best as a collective.", "effects": {"right": -1}},
-
     {"id": 1, "question": "The collective can be a burden on the individual.", "effects": {"right": 1}},
-
     {"id": 2, "question": "Utillitarianism is a good moral frame work.", "effects": {"right": -1}},
-
     {"id": 3, "question": ". If people want to live unhealthy lifestyles, it is their right to do so.", "effects": {"right": 1}},
-
-   
     {"id": 5, "question": "Planned economies are better than market ones.", "effects": {"right": -1}},
-
     {"id": 6, "question": "People are too obsessed with having a comfortable life as opposed to thinking more about how they could contribute.", "effects": {"right": -1}},
-
     {"id": 7, "question": "Individual goals are more important than group goals.", "effects": {"right": 1}},
-
     {"id": 8, "question": "Cooperation is more important than independence.", "effects": {"right": -1}},
-
     {"id": 9, "question": "No individual should ever be required to serve any collective expectation.", "effects": {"right": 1}},
-
     {"id": 10, "question": "I consider myself an egoist.", "effects": {"right": 1}},
-
     {"id": 11, "question": "Authority corrupts both ruler and ruled ones.", "effects": {"auth": -1}},
-
     {"id": 12, "question": "The truth is, no one of us can be truly free until everybody is free.", "effects": {"auth": -1}},
-
     {"id": 13, "question": "It is in the individual's interest to help others.": {"right": -1}},
-
     {"id": 14, "question": "Economic inequality is too high in the world.", "effects": {"auth": -1}},
-
     {"id": 15, "question": "Wealth accumulation leads to the creation of an oppressive class system.", "effects": {"auth": -1}},
-
     {"id": 16, "question": "Hierarchies should be dismantled.", "effects": {"auth": -1}},
-
     {"id": 17, "question": "It is to be expected to see social hierarchy remain, as anyone can be better or worse than others.", "effects": {"auth": 1}},
-
     {"id": 18, "question": "An economy is generally designed better when it is organized bottom-up rather than top-down.", "effects": {"auth": -1}},
-
     {"id": 19, "question": "Society requires structure and some power inequality in order to function correctly.", "effects": {"auth": 1}},
-
     {"id": 20, "question": "People who can, but choose not to work, should not get any help from others.", "effects": {"auth": 1}},
-
     {"id": 21, "question": "It is foolish to seek to do away with inequalities based on social standing and class.", "effects": {"auth": 1}},
-
     {"id": 22, "question": "Slavery can be justified if it's productive enough.", "effects": {"auth": 1}},
-
     {"id": 23, "question": "We should move beyond wage systems.", "effects": {"auth": -1}},
-
     {"id": 24, "question": "Helping others out by doing good deeds is its own reward.", "effects": {"right": -1}},
-
     {"id": 25, "question": "All economic entities should serve the intrest of the society as a whole.", "effects": {"right": -1}},
-
     {"id": 26, "question": "An individual's body is their own property, and they should be able to do anything they desire to it.", "effects": {"prog": 1}},
-
     {"id": 27, "question": "Multiculturalism is bad.", "effects": {"prog": -1}}
-
 ];
